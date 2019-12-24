9a17ecef2cb09f7e632defa3a74691464b869d5d
Veerabhadra rao Uppu <38528121+uvbr@users.noreply.github.com>
2019-12-24 15:35:46 +0530

    Delete arithmetic.py

--- progression/linear/arithmetic.py
+++
@@ -1,8 +0,0 @@
-
-class Arithmetic:
-    def __init__(self, first_term, common_diff):
-        self.first_term= first_term
-        self.common_diff= common_diff
-
-    def nth_term(self, nth_pos):
-        return self.first_term+ (nth_pos-1)*self.common_diff
