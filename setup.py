b25dadc1c1a4180e83871e25512cadac99cac8d0
veera <veerabhadrarao.uppu@nexiilabs.com>
2019-12-24 16:14:19 +0530

    trying to resolve

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='asdfg',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['documentation.txt']),
     package_dir={'':'asdfg'},
     url="http://www.example.com",
@@ -10,5 +10,5 @@ setup(
     author='veera',
     author_email='veerabhadrarao.uppu@nexiilabs.com',
     description='asdfg_description',
-    download_url="https://github.com/uvbr/package_creation/archive/0.1.tar.gz"
+    download_url="https://github.com/uvbr/package_creation/archive/0.2.tar.gz"
 )
