47399696f62086651c6705a270827f66a039225e
veera <veerabhadrarao.uppu@nexiilabs.com>
2019-12-26 18:07:57 +0530

    0.4

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup,find_packages
 
 setup(
     name='asdfg',
-    version='0.3',
+    version='0.4',
     packages=find_packages(exclude=['documentation.txt']),
-    package_dir={'':'asdfg'},
+    # package_dir={'':''},
     url="http://www.example.com",
     license='',
     author='veera',
     author_email='veerabhadrarao.uppu@nexiilabs.com',
     description='asdfg_description',
-    download_url="https://github.com/uvbr/package_creation/archive/0.3.tar.gz"
+    download_url="https://github.com/uvbr/package_creation/archive/0.4.tar.gz"
 )
